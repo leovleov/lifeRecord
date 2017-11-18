b240342526621019f12174958c39301ae5bcd108
Sheng-Po Tseng <sheng.po.tseng@sv.cmu.edu>
2017-11-18 00:42:37 -0800

    Add cancel at sign up page. Add some target APIs.

--- src/main/java/cmu/sv/lifeRecord/rest/TargetInterface.java
+++ src/main/java/cmu/sv/lifeRecord/rest/TargetInterface.java
@@ -4,10 +4,13 @@ import cmu.sv.lifeRecord.exceptions.APPBadRequestException;
 import cmu.sv.lifeRecord.exceptions.APPInternalServerException;
 import cmu.sv.lifeRecord.exceptions.APPNotFoundException;
 import cmu.sv.lifeRecord.exceptions.APPUnauthorizedException;
+import cmu.sv.lifeRecord.helpers.APPCrypt;
 import cmu.sv.lifeRecord.helpers.APPResponse;
 import cmu.sv.lifeRecord.helpers.AuthCheck;
 import cmu.sv.lifeRecord.models.Album;
 import cmu.sv.lifeRecord.models.Target;
+import cmu.sv.lifeRecord.models.Token;
+import cmu.sv.lifeRecord.models.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -17,6 +20,7 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.json.JSONObject;
 
 import javax.ws.rs.*;
@@ -26,12 +30,15 @@ import javax.ws.rs.core.MediaType;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.regex.Pattern;
 
 @Path("targets")
 public class TargetInterface {
     private MongoCollection<Document> albumCollection;
     private MongoCollection<Document> targetCollection;
+    private MongoCollection<Document> editorCollection;
+    private MongoCollection<Document> watcherCollection;
     private ObjectWriter ow;
 
 
@@ -41,8 +48,46 @@ public class TargetInterface {
 
         this.albumCollection = database.getCollection("albums");
         this.targetCollection = database.getCollection("targets");
+        this.editorCollection = database.getCollection("editors");
+        this.watcherCollection = database.getCollection("watchers");
         ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public APPResponse getOne(@Context HttpHeaders headers, @PathParam("id") String id) {
+        try {
+            AuthCheck.checkEditorOrWatcherAuthentication(headers,id);
+            BasicDBObject query = new BasicDBObject();
+
+            query.put("_id", new ObjectId(id));
+            Document item = targetCollection.find(query).first();
+            if (item == null) {
+                throw new APPNotFoundException(0, "No such target.");
+            }
+            Target target = new Target(
+                    item.getString("targetName"),
+                    item.getString("targetInfo"),
+                    item.getString("creatorId")
+            );
+            target.setId(item.getObjectId("_id").toString());
+            return new APPResponse(target);
+
+        }
+        catch(APPNotFoundException e) {
+            throw e;
+        }
+        catch(APPUnauthorizedException e) {
+            throw e;
+        }
+        catch(IllegalArgumentException e) {
+            throw new APPBadRequestException(45,"Unacceptable ID.");
+        }
+        catch(Exception e) {
+            throw new APPInternalServerException(99,"Unexpected error!");
+        }
     }
 
     @GET
@@ -83,6 +128,70 @@ public class TargetInterface {
         }
         catch(Exception e) {
             throw new APPInternalServerException(99,"Unexpected error!");
+        }
+    }
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
+    public APPResponse create(@Context HttpHeaders headers, Object request) {
+
+        JSONObject json = null;
+        try {
+            AuthCheck.checkAnyAuthentication(headers);
+            List<String> authHeaders = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
+            if (authHeaders == null)
+                throw new APPUnauthorizedException(70, "No Authorization Headers");
+            String token = authHeaders.get(0);
+            String userId = APPCrypt.decrypt(token);
+
+            json = new JSONObject(ow.writeValueAsString(request));
+            if (!json.has("targetName"))
+                throw new APPBadRequestException(55, "Missing target name.");
+            if (!json.has("targetInfo"))
+                throw new APPBadRequestException(55, "Missing target Info.");
+            BasicDBObject query = new BasicDBObject();
+
+            Document doc = new Document("targetName", json.getString("targetName"))
+                    .append("targetInfo", json.getString("targetInfo"))
+                    .append("creatorId", userId);
+            targetCollection.insertOne(doc);
+
+            BasicDBObject query2 = new BasicDBObject();
+            query2.put("targetName", json.getString("targetName"));
+            query2.put("creatorId", userId);
+            Document item2 = targetCollection.find(query2).first();
+            if (item2 == null) {
+                throw new APPNotFoundException(0, "No such target.");
+            }
+            Target target = new Target(
+                    item2.getString("targetName"),
+                    item2.getString("targetInfo"),
+                    item2.getString("creatorId")
+            );
+            target.setId(item2.getObjectId("_id").toString());
+
+            Document doc2 = new Document();
+            doc2.append("userId", userId);
+            doc2.append("targetId", item2.getObjectId("_id").toString());
+            doc2.append("isCreator", true);
+            editorCollection.insertOne(doc2);
+
+            Document doc3 = new Document();
+            doc3.append("userId", userId);
+            doc3.append("targetId", item2.getObjectId("_id").toString());
+            watcherCollection.insertOne(doc3);
+
+            return new APPResponse(target);
+
+        } catch (JsonProcessingException e) {
+            throw new APPBadRequestException(33, e.getMessage());
+        } catch(APPUnauthorizedException e) {
+            throw e;
+        } catch (APPBadRequestException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new APPInternalServerException(99, "Unexpected error!");
         }
     }
 
