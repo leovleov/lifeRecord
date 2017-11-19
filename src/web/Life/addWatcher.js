436f9186d33fda54b41401ac5a30a994b4936053
miamiao <Zm880302>
2017-11-19 08:51:47 -0800

    Add Watcher related JS

---
+++ src/web/Life/addWatcher.js
@@ -0,0 +1,103 @@
+$(function() {
+    var token = localStorage.getItem("token");
+    var userId = localStorage.getItem("userId");
+    var userSelect = document.getElementById('userIdSelect');
+    var targetSelect = document.getElementById('targetIdSelect');
+
+    $("#greeting").hide();
+
+    jQuery.ajax({
+        url: "/rest/users/"+userId+"/targets",
+        type: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", token);
+        }
+    }).done(function (data) {
+        var dataList = data.content;
+
+        for (var i = 0; i < dataList.length; i++) {
+            var targetName = dataList[i].targetName;
+            var targetInfo = dataList[i].targetInfo;
+            var targetId = dataList[i].id;
+            var opt = document.createElement('option');
+            opt.value = targetId;
+            opt.innerHTML = targetName;
+            targetSelect.appendChild(opt);
+        }
+    })
+
+    $('#addButton').click(function () {
+        jQuery.ajax ({
+            url:  "/rest/watchers/",
+            type: "POST",
+            async: false,
+            data: JSON.stringify({userId:userSelect.options[userSelect.selectedIndex].value,targetId:targetSelect.options[targetSelect.selectedIndex].value}),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader ("Authorization", localStorage.getItem("token"));
+            }
+        }).done(function(data){
+            $("#greeting").text("Add a new watcher " + userSelect.options[userSelect.selectedIndex].innerHTML + " successful!");
+            $("#greeting").show();
+            location.href = "WatchersList.html"
+        }).fail(function(data){
+            $("#greeting").text("Fail ! Please check the data!");
+            $("#greeting").show();
+        })
+    })
+    $('#cancelButton').click(function () {
+        location.href = "WatchersList.html"
+    })
+})
+
+function targetChange() {
+
+    var token = localStorage.getItem("token");
+    var userId = localStorage.getItem("userId");
+    var userSelect = document.getElementById('userIdSelect');
+    var targetSelect = document.getElementById('targetIdSelect');
+    for(var i = userSelect.options.length - 1 ; i >= 0 ; i--)
+    {
+        userSelect.remove(i);
+    }
+    jQuery.ajax({
+        url: "/rest/users/",
+        type: "GET",
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader ("Authorization", token);
+        // }
+    }).done(function (data) {
+        var dataList = data.content;
+        var watcherList;
+        $.ajax({
+            url:  "/rest/targets/"+targetSelect.options[targetSelect.selectedIndex].value+"/watchers",
+            type: "GET",
+            async: false,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader ("Authorization", localStorage.getItem("token"));
+            }
+        }).done(function(data){
+            watcherList = data.content;
+        }).fail(function(data){
+            watcherList = null;
+        })
+
+        for (var i = 0; i < dataList.length; i++) {
+            var name = dataList[i].firstName + " " + dataList[i].lastName;
+            var watcherId = dataList[i].id;
+            var found = false;
+            for(var j = 0 ; j < watcherList.length ; j++){
+                if(watcherId == watcherList[j].id || watcherId == "59fc1ddfa69f5d401c607623"){
+                    found = true;
+                }
+            }
+            if(found == false) {
+                var opt = document.createElement('option');
+                opt.value = watcherId;
+                opt.innerHTML = name;
+                userSelect.appendChild(opt);
+            }
+        }
+    })
+}
--- src/web/Life/watchers.js
+++ src/web/Life/watchers.js
@@ -15,7 +15,7 @@ $(function() {
             var targetName = dataList[i].targetName;
             var targetInfo = dataList[i].targetInfo;
             var targetId = dataList[i].id;
-            var watchersList;
+            var watcherList;
             $.ajax({
                 url:  "/rest/targets/"+targetId+"/watchers",
                 type: "GET",
@@ -27,63 +27,50 @@ $(function() {
                 watcherList = data.content;
             }).fail(function(data){
                 watcherList = null;
-                //localStorage.setItem("isAdmin",false);
             })
 
             var watcherDiv = '<div class="row col-md-8 col-md-offset-2">\n' +
-                '        <form class="form-signin">\n' +
+                '        <form class="form-signin" >\n' +
                 '            <h2 class="form-signin-heading">Target: '+targetName+'</h2>\n';
 
             for(var j = 0 ; j < watcherList.length ; j++){
                 watcherDiv = watcherDiv +
                     '<div class="form-check">\n' +
                     '  <label class="form-check-label">\n' +
-                    '    <input class="form-check-input" type="checkbox" name="watcherCheck" id="watcherCheck" value="'+watcherList[j].id+'">\n' +
-                    watcherList[j].userId +
+                    '    <input class="form-check-input" type="checkbox" id = "'+watcherList[j].id+'_'+i+'" name="watcherCheck" value="'+targetId+'">\n' +
+                    watcherList[j].firstName + " " + watcherList[j].lastName +
                     '  </label>\n' +
                     '</div>';
 
-                // '<div class="checkbox">\n' +
-                // '                <label>\n' +
-                // '                    <input type="checkbox" value="'+albumList[j].albumName+'"> '+albumList[j].albumName+'\n' +
-                // '                </label>\n' +
-                // '            </div>';
             }
 
             watcherDiv = watcherDiv +    '        </form>' +
                 '</div>'
             $('#watcherList').append(watcherDiv);
 
+
         }
 
     })
 
-    function getCheckedBoxes() {
-        var checkboxes = document.getElementsByName(watcherCheck);
-        var checkboxesChecked = [];
-        // loop over them all
-        for (var i=0; i<checkboxes.length; i++) {
-            // And stick the checked ones onto an array...
-            if (checkboxes[i].checked) {
-                checkboxesChecked.push(checkboxes[i]);
-            }
-        }
-        // Return the array if it is non-empty, or null
-        return checkboxesChecked.length > 0 ? checkboxesChecked : null;
-    }
+
 
     $('#addWatcher').click(function () {
         location.href = "AddWatcher.html"
     })
 
     $('#deleteWatcher').click(function () {
-        var checkboxesChecked = document.querySelectorAll('input[name=WatcherCheck]:checked');
+        var checkboxesChecked = document.querySelectorAll('input[name=watcherCheck]:checked');
         //var checkboxesChecked = getCheckedBoxes();
         localStorage.setItem("checkBoxNum",checkboxesChecked.length);
         for(var i = 0 ; i < checkboxesChecked.length ; i++) {
             jQuery.ajax({
-                url: "/rest/Watchers/"+checkboxesChecked[i].value,
+                url: "/rest/watchers/",
                 type: "DELETE",
+                async: false,
+                data: JSON.stringify({userId:checkboxesChecked[i].id.split("_")[0],targetId:checkboxesChecked[i].value}),
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader("Authorization", token);
                 }
